7e3ad530277d4dcea1d790a30fecc49ff4dcd340
DulranSam <dulransamarasinghe4@gmail.com>
2025-05-28 01:52:36 +0530

    fwq

--- ex.py
+++ ex.py
@@ -31,7 +31,8 @@ job_embeddings = model.encode(jobs['text'].tolist())
 
 # Build FAISS index
 # get the first resume embedding vector , then the size of the first dimension
-d = resume_embeddings[0].shape[0] # d is assigned the length of the first resume embedding vector
+# d is assigned the length of the first resume embedding vector
+d = resume_embeddings[0].shape[0]
 # IndexFlatL2 is used for L2 distance (Euclidean distance) search
 index = faiss.IndexFlatL2(d)
 # add to index array of resume embeddings
@@ -40,6 +41,7 @@ index.add(np.array(resume_embeddings))
 # Search: top 3 resumes per job
 for i, job_vec in enumerate(job_embeddings):
     # D is distances, I is indices of nearest neighbors
+    # index.search takes a numpy array of job_vec and k is the number of nearest neighbors to find
     D, I = index.search(np.array([job_vec]), k=3)
     print(f"\nTop 3 Resumes for Job {i+1}:\n")
     for idx in I[0]:  # I[0] contains indices of the top 3 resumes , idx is the index of the resume in the resumes DataFrame
